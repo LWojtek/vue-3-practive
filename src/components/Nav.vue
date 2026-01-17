3c12a8bf53192a7ea6fbefb7446d9dc6a459134a
Wojtek Lehmann <wojciech.lehmann@grupaang.pl>
2026-01-17 14:19:59 +0100

    Feature: navigation

---
+++ src/components/Nav.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="self-stretch">
+    <ul class="flex items-stretch gap-4 h-full">
+      <li class="flex items-stretch">
+        <a
+          href="/"
+          class="text-gray-500 flex items-center hover:text-black transition border-b-2 border-transparent hover:border-amber-500"
+          >Menu</a
+        >
+      </li>
+      <li class="flex items-stretch">
+        <a
+          href="/"
+          class="text-gray-500 flex items-center hover:text-black transition border-b-2 border-transparent hover:border-amber-500"
+          >Locations</a
+        >
+      </li>
+      <li class="flex items-stretch">
+        <a
+          href="/"
+          class="text-gray-500 flex items-center hover:text-black transition border-b-2 border-transparent hover:border-amber-500"
+          >About</a
+        >
+      </li>
+      <li class="flex items-stretch">
+        <a
+          href="/"
+          class="text-gray-500 flex items-center hover:text-black transition border-b-2 border-transparent hover:border-amber-500"
+          >Contact</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
